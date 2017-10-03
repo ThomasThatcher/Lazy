4fa7f4229f496fbdd4fa33a7c988d6bf9236cc1c
Tom <tomthatcher96@gmail.com>
2017-10-04 10:41:49 +1100

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ import subprocess
 import sys
 import re
 
+# Commands
+restart_litespeed = "service lsws restart"
+backup_commands = " df -h; cd /home; rm -rf */backup-*; rm -rf */application_backups; rm -rf */softaculous_backups; rm -f */public_html/error_log*; df -h;"
+
 host    = sys.argv[1]
 command = sys.argv[2]
 
@@ -20,7 +24,9 @@ def remote_ssh_command(c):
 		print "Output:" + stdout
 
 if command = "restartls":
-	remote_ssh_command("service lsws restart")
+	remote_ssh_command(restart_litespeed)
+elif command = "bk":
+	remote_ssh_command(backup_commands)
 else:
 	sys.stderr.write("Error: Invalid input")
 
